e99fc079df048f1d081b8a28b562759055456330
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-03-31 12:10:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 requirements = ["numpy>=1.17", "scipy>=1.4.1", "matplotlib>=3.2.1", "jupyter>=1.0.0", "scikit-image>=0.16.2",
-                "xrt", "gpytorch", "botorch", "xopt", "minepy"]
+                "xrt", "xopt]
 
 setup(
-    name="lcls_beamline_optimization",
-    version="0.0.2",
-    author="AAM",
-    author_email="--",
-    description="Tools for LCLS beamline Optimization",
+    name="lcls_beamline_toolbox",
+    version="0.0.1",
+    author="",
+    author_email="",
+    description="tools for LCLS beamline calculations",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/aashwinmishra/lcls_beamline_optimization",
+    url="https://github.com/aashwinmishra/SLACBeamOptimization",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
