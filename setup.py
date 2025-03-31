57b3ca933daf7288f2a31a5f1ecd261d55407bb9
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-03-31 13:11:50 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+# with open("README.md", "r") as readme_file:
+#     readme = readme_file.read()
 
 requirements = ["numpy>=1.17", "scipy>=1.4.1", "matplotlib>=3.2.1", "jupyter>=1.0.0", "scikit-image>=0.16.2",
                 "xrt", "xopt]
@@ -12,8 +12,8 @@ setup(
     author="",
     author_email="",
     description="tools for LCLS beamline calculations",
-    long_description=readme,
-    long_description_content_type="text/markdown",
+    # long_description=readme,
+    # long_description_content_type="text/markdown",
     url="https://github.com/aashwinmishra/lcls_beamline_toolbox",
     packages=find_packages(),
     install_requires=requirements,
